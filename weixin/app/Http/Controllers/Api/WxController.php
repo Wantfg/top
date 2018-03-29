db872aeb1c65a86afa8ec3074f0d639f9a044ceb
wantfg <wantfg@163.com>
2018-03-29 13:57:49 +0800

    配置回复

--- weixin/app/Http/Controllers/Api/WxController.php
+++ weixin/app/Http/Controllers/Api/WxController.php
@@ -34,9 +34,43 @@ class WxController extends Controller
     //
     public function index()
     {
-        $response = $this->app->server->serve();
+        //配置服务----------
+//        $response = $this->app->server->serve();
         // 将响应输出
-        return $response; // Laravel 里请使用：return $response;
+//        return $response; // Laravel 里请使用：return $response;
+        //配置服务----------END
+
+        $this->app->server->push(function ($message) {
+            switch ($message['MsgType']) {
+                case 'event':
+                    return '收到事件消息';
+                    break;
+                case 'text':
+                    return '收到文字消息';
+                    break;
+                case 'image':
+                    return '收到图片消息';
+                    break;
+                case 'voice':
+                    return '收到语音消息';
+                    break;
+                case 'video':
+                    return '收到视频消息';
+                    break;
+                case 'location':
+                    return '收到坐标消息';
+                    break;
+                case 'link':
+                    return '收到链接消息';
+                    break;
+                // ... 其它消息
+                default:
+                    return '收到其它消息';
+                    break;
+            }
+
+            // ...
+        });
 
     }
 }
