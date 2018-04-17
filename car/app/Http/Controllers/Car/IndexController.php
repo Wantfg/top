8e36be056e76b35d12f1b0bdd5103f5516949270
wantfg <wantfg@163.com>
2018-04-17 18:32:52 +0800

    22

--- car/app/Http/Controllers/Car/IndexController.php
+++ car/app/Http/Controllers/Car/IndexController.php
@@ -2,7 +2,6 @@
 
 namespace App\Http\Controllers\Car;
 
-use Illuminate\Http\Request;
 use App\Http\Controllers\Controller;
 use Illuminate\Support\Facades\DB;
 
@@ -16,8 +15,8 @@ class IndexController extends Controller
         //pattern--------------------------------
         $abc_pattern = '/<div vos="gs" class="uibox" id="(?:\w+)" style(?:=""|="display:none")>(.*?)<\/div>\s*<\/div>/ism';
         $brand_abc_pattern = '/<div class="uibox-title uibox-title-border" data="(?:.*)"><span class="font-letter">(\w{1})<\/span><\/div>/';
-        $brand_name_pattern = '/<dl id="(?:\d+)" olr="(?:\d+)">(.*?)<\/dl>/ism';
-        $brand_name_zh_pattern = '/<div><a href="https:\/\/car.autohome.com.cn\/pic\/brand-(?:\d+).html">(.*?)<\/a><\/div>/ims';
+        $brand_area_pattern = '/<dl id="(?:\d+)" olr="(?:\d+)">(.*?)<\/dl>/ism';
+        $brand_name_pattern = '/<div><a href="https:\/\/car.autohome.com.cn\/pic\/brand-(?:\d+).html">(.*?)<\/a><\/div>/ims';
         $brand_name_factory_pattern = '/<div class="h3-tit">(.*?)<\/div>/';
         $brand_series_pattern = '/<ul class="rank-list-ul" 0="">(.*?)<\/ul>/s';
         $in_sale_series_pattern = '/<h4(?: class="toolong")?><a href="https:\/\/www.autohome.com.cn\/(\d+)\/#levelsource=000000000_0&amp;pvareaid=101594">(.*?)(?:<\/a><a href="(?:.*)" target="_blank" class="icon-double11">特卖)?<\/a><\/h4>/';
@@ -31,12 +30,12 @@ class IndexController extends Controller
             preg_match($brand_abc_pattern,$brand_each,$brand_abc_row);
             $car_info[$k]['brand_abc'] = $brand_abc_row[1];
             //3、以品牌区分
-            preg_match_all($brand_name_pattern,$brand_each,$brand_name_row);
+            preg_match_all($brand_area_pattern,$brand_each,$brand_name_row);
             //4、查询出品牌名称
             foreach ($brand_name_row[0] as $kk => $brand_name_each){
                 $in_sale_series_ = $not_sale_series_ = $series = [];
-                preg_match($brand_name_zh_pattern,$brand_name_each,$brand_name);
-                $brand_name_zh = $brand_name[1]; //品牌中文名
+                preg_match($brand_name_pattern,$brand_name_each,$brand_name_row);
+                $brand_name = $brand_name_row[1]; //品牌中文名
                 preg_match_all($brand_name_factory_pattern,$brand_name_each,$brand_name_factory);
                 $brand_name_factory = $brand_name_factory[1]; //品牌旗下的厂商名称
                 preg_match_all($brand_series_pattern,$brand_name_each,$brand_series);
@@ -45,27 +44,66 @@ class IndexController extends Controller
                     preg_match_all($not_sale_series_pattern,$it,$not_sale_series);
                     //将车型的汽车之家id设置为键值
                     foreach ($in_sale_series[2] as $id_K => $item){
-                        $in_sale_series_[] = ['autohome_id' => $in_sale_series[1][$id_K],'name' => $in_sale_series[2][$id_K]];
+                        $series[] = ['autohome_id' => $in_sale_series[1][$id_K],'name' => $in_sale_series[2][$id_K],'in_sale' => 1];
                     }
                     foreach ($not_sale_series[2] as $id_K => $item){
-                        $not_sale_series_[] = ['autohome_id' => $not_sale_series[1][$id_K],'name' => $not_sale_series[2][$id_K]];
+                        $series[] = ['autohome_id' => $not_sale_series[1][$id_K],'name' => $not_sale_series[2][$id_K],'in_sale' => 0];
                     }
 
-                        $series[] = array(
-                            'in_sale' => $in_sale_series_,
-                            'not_sale' => $not_sale_series_
-                        );
                 }
                 $car_info[$k]['brand_info'][] = array(
-                    'brand_name_zh' => $brand_name_zh,
+                    'brand_name' => $brand_name,
                     'brand_factory' => $brand_name_factory,
                     'series' => $series
                 );
+                //保存品牌
+                $brand_id = DB::table('car_brand')
+                    ->where('brand_name',$brand_name)
+                    ->value('id');
+                if(empty($brand_id)){
+                    $brand_id = DB::table('car_brand')->insertGetId([
+                        'brand_abc' => $car_info[$k]['brand_abc'],
+                        'brand_name' => $brand_name
+                    ]);
+                }
+                //保存厂商
+                foreach ($brand_name_factory as $item){
+                    $factory_id = DB::table('car_factory')
+                        ->where('brand_id',$brand_id)
+                        ->where('factory_name',$item)
+                        ->value('id');
+                    if(empty($factory_id)){
+                        $factory_id = DB::table('car_factory')->insertGetId([
+                            'brand_id' => $brand_id,
+                            'factory_name' => $item
+                        ]);
+                    }
+                    //保存车系名称
+                    foreach ($series as $s){
+                        $series_id = DB::table('car_series')
+                            ->where('brand_id',$brand_id)
+                            ->where('factory_id',$factory_id)
+                            ->where('autohome_id',$s['autohome_id'])
+                            ->where('series_name',$s['name'])
+                            ->value('id');
+                        if(empty($series_id)){
+                            $series_id = DB::table('car_series')->insertGetId([
+                                'brand_id' => $brand_id,
+                                'factory_id' => $factory_id,
+                                'autohome_id' => $s['autohome_id'],
+                                'series_name' => $s['name'],
+                                'in_sale' => $s['in_sale'],
+                            ]);
+                        }
+                    }
+                }
+
             }
         }
-        $a = DB::table('car_brand')->get();
-
-        var_dump($a);
+//        var_dump($car_info);
+//        $a = DB::table('car_brand')->get();
+//
+//        var_dump($a);
 
     }
 }
