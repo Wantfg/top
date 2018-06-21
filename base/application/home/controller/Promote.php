d53087473459e6856c2be037756d3de68a9f7627
Wantfg <wantfg@gmail.com>
2018-06-22 01:22:23 +0800

    ww

---
+++ base/application/home/controller/Promote.php
@@ -0,0 +1,29 @@
+<?php
+// +----------------------------------------------------------------------
+// | TwoThink [ WE CAN DO IT JUST THINK IT ]
+// +----------------------------------------------------------------------
+// | Copyright (c) 2013 http://www.twothink.cn All rights reserved.
+// +----------------------------------------------------------------------
+// | Author: 麦当苗儿 <zuojiazi@vip.qq.com> <http://www.zjzit.cn>
+// +----------------------------------------------------------------------
+
+namespace app\home\controller;
+use app\home\model\Document;
+/**
+ * 展示页
+ * 
+ */
+class Promote extends Home{
+
+
+	//留空默认页
+    public function index(){
+
+    }
+
+    public function ()
+    {
+        
+    }
+
+}
